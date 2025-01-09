1cd41e8c7cf1bb50683b0339c04b6ef0e7e429bf
Bence1102 <szente.bence04@gmail.com>
2025-01-09 11:24:59 +0100

    2es feladat kész

--- sorozat.py
+++ sorozat.py
@@ -1,7 +1,12 @@
 import random
+    
+    
+    
+global szamok    
+szamok=[]
+
 def sor():
     print("II/A,B,C:")
-    szamok=[]
     for _ in range(5):
         veletlenszam =random.randint(10,20)   
         szamok.append(veletlenszam)
@@ -11,19 +16,19 @@ def sor():
             print("-", end="")
         print(szamok[i], end="")
     print()  
+    return szamok
 
 
-def kisebb(szamok):
-    print("II/D:")
+def kisebb():
     count = 0
-    for i in range(1, len(szamok)):
+    for i in range(1, len(szamok),1):
         if szamok[i] > szamok[i-1]:  
             count += 1
     return count
 
-def konzolba_ir(szamok):
-    print("II/E:")
-    eredmeny = kisebb(szamok)  
+def konzolba_ir():
+    print("II/D, E:")
+    eredmeny = kisebb()  
     print(f"A kisebb függvény eredménye: {eredmeny}")
 
 def fajlba_ir(szamok):
